c03ad8a0cf10e51b8954759c17b4e4a294d64979
Mesut Kutlu <mkutlu@etu.edu.tr>
2018-05-09 02:09:59 +0300

    City name-href  info json formatted

--- getCities.py
+++ getCities.py
@@ -1,4 +1,5 @@
 import requests
+import json
 from bs4 import BeautifulSoup
 
 page = requests.get("https://www.yemeksepeti.com/sehir-secim")
@@ -9,8 +10,21 @@ f.write(soup.prettify())
 
 cities = soup.find_all('a', class_='cityLink')
 
+names, hrefs = [], []
 for city in cities:
-	city_name = city.find_all('span', class_='name')
+	city_name_span = city.find('span', class_='name')
+	city_name = city_name_span.get_text()
+	city_href = city['href']	
 	print('{}{}'.format(city_name,"\n"))
+	city_data = {}
+	names.append(city_name)
+	hrefs.append(city_href)
+json_data = [{"name" : n, "href" : h} for n,h in zip(names,hrefs)]	
+#city_data['city_name'] = city_name
+#city_data['city_href'] = city_href
+#json_data = json.dumps(city_data)
+with open('cities.json', 'w') as f:
+    json.dump(json_data, f)
+
 
 
