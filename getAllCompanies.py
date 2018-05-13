165a758f51d509c7cc092fd4ac4bceb4aba2a6ec
Mesut Kutlu <mkutlu@etu.edu.tr>
2018-05-14 01:11:36 +0300

    retrieve all company list by zones

--- getAllCompanies.py
+++ getAllCompanies.py
@@ -4,8 +4,16 @@ import requests
 import json
 from bs4 import BeautifulSoup
 import ast
+import datetime
 
 def getAllCompaniesFromZone():
+
+    #json file company attrs
+    DisplayName,CategoryName,CuisineNameList,DeliveryTime,MinimumDeliveryPrice,ImageFullPath,MainCuisineId,MainCuisineName,ServingText,href,SpeedText,FlavourText,AvgPoint,WorkHoursText = [], [], [], [], [], [], [], [], [], [], [], [], [], []
+
+    #json file location attrs
+    allZones,aCity, aZone, cityZones = [], [], [],[]
+
     with open('zones.json', 'r') as f:
         zones_json = json.load(f)
 
@@ -25,45 +33,40 @@ def getAllCompaniesFromZone():
                         print(zones_json['allZones'][count][key][count2][key2][0]['name'])
                         for zone in zones_json['allZones'][count][key][count2][key2]:
                             page = requests.get('https://www.yemeksepeti.com{}'.format(zone['url']))
+                            print(zone['url'])
                             soup = BeautifulSoup(page.content, 'html.parser')
                             companies = soup.find_all('a', class_='restaurantName')
                             for company in companies:
                                 company_span = company.find('span')
                                 company_name = company_span.get_text()
                                 company_datatooltip =  json.loads(company_span['data-tooltip'])
-                                print(company_datatooltip['CategoryName'])
-                                '''for k,v in company_datatooltip.items():
-                                    if(k is "CategoryName"):
-                                        print(v)'''
+                                DisplayName.append(company_datatooltip['DisplayName'])
+                                CategoryName.append(company_datatooltip['CategoryName'])
+                                CuisineNameList.append(company_datatooltip['CuisineNameList'])
+                                DeliveryTime.append(company_datatooltip['DeliveryTime'])
+                                MinimumDeliveryPrice.append(company_datatooltip['MinimumDeliveryPrice'])
+                                ImageFullPath.append(company_datatooltip['ImageFullPath'])
+                                MainCuisineId.append(company_datatooltip['MainCuisineId'])
+                                MainCuisineName.append(company_datatooltip['MainCuisineName'])
+                                ServingText.append(company_datatooltip['ServingText'])
+                                SpeedText.append(company_datatooltip['SpeedText'])
+                                FlavourText.append(company_datatooltip['FlavourText'])
+                                AvgPoint.append(company_datatooltip['AvgPoint'])
+                                WorkHoursText.append(company_datatooltip['WorkHoursText'])
+                                href.append(company['href'])                       
+                            aZone.append({ "comps" : [{"DisplayName" : dn, "CategoryName" : cn,"CuisineNameList" : cnl, "DeliveryTime" : dt,"MinimumDeliveryPrice" : mdp, "ImageFullPath" : ifp,"MainCuisineId" : mci, "MainCuisineName" : mcn,"ServingText": st, "SpeedText": spt,"FlavourText": ft,"AvgPoint" : ap, "WorkHoursText":wht, "href" : h} for dn,cn,cnl,dt,mdp,ifp,mci,mcn,st,spt,ft,ap,wht,h in zip(DisplayName,CategoryName,CuisineNameList,DeliveryTime,MinimumDeliveryPrice,ImageFullPath,MainCuisineId,MainCuisineName,ServingText,SpeedText,FlavourText,AvgPoint,WorkHoursText,href)]})
+                            DisplayName,CategoryName,CuisineNameList,DeliveryTime,MinimumDeliveryPrice,ImageFullPath,MainCuisineId,MainCuisineName,ServingText,href,SpeedText,FlavourText,AvgPoint,WorkHoursText = [], [], [], [], [], [], [], [], [], [], [], [], [], []
+                            print(zone['name'])
+                            cityZones.append({zone['name'] : aZone})
+                            aZone = []
+                aCity.append({ key : cityZones })
+                cityZones = []
+    jsondata = {"allZones" : aCity}
+    print(jsondata)
+    with open('companies.json', 'w') as f:
+            json.dump(jsondata, f, ensure_ascii=False, indent=4, sort_keys=True)
+
+
+                                
 
 getAllCompaniesFromZone()
-            
-            
-'''<span data-tooltip="{ ;AreaName ;: ;Acıbadem ;, ;CatalogName ;: ;TR_ISTANBUL ;,
-                             ;CategoryName ;: ;2b57297a-d0cc-433b-9163-49147e471d8d ;
-                             , ;ClosedByParent ;:false, ;CuisineNameList ;:[ ;Kahvaltı ;, ;Cafe ;, ;Pasta \u0026 Tatlı ;],
-                              ;DeliveryFee ;:0, ;DeliveryTime ;:45, ;DisplayName ;: ;G&#246;reme Muhallebicisi, Acıbadem ;
-                              , ;Flavour ;:9.24, ;HasDVDPromotion ;:false, ;ImageLabel ;:null, ;ImageLabelList ;:[ ;cc_genel.gif ;], 
-                              ;ImagePath ;: ;/CategoryImages/TR_ISTANBUL/goreme_muhalllebicisi_kurtulus.gif ;
-                              , ;ImageFullPath ;: ;http://images.yemeksepeti.com//CategoryImages/TR_ISTANBUL/goreme_muhalllebicisi_kurtulus_big.gif ;
-                              , ;ImageLabelListFullPath ;:[ ;http://images.yemeksepeti.com/Labels/Restaurant/cc_genel.gif ;],
-                               ;IsOpen ;:true, ;IsRestaurantOpen ;:true, ;MainCuisineId ;: ;96f07b33-348d-4848-b428-005cf224f9c3 ;,
-                                ;MainCuisineLabelName ;: ;Pasta.jpg ;, ;MainCuisineName ;: ;Pasta
-                                 \u0026 Tatlı ;, ;MinimumDeliveryPrice ;:5.0, ;Oid ;:0, ;OpenRestaurantCount ;:0,
-                                  ;PaymentMethodsText ;: ;Ticket Restaurant Yemek Kartı^Nakit^Multinet^Online Kredi/Banka Kartı^Ticket Restaurant Yemek
-                                   &#199;eki^SetCard^MetropolCard^Sodexo Yemek &#199;eki^Sodexo Yemek Kartı^Kredi Kartı^ ;, ;PaymentMethodsList ;:
-                                   [ ;Ticket Restaurant Yemek Kartı ;, ;Nakit ;, ;Multinet ;, ;Online Kredi/Banka Kartı ;, ;Ticket Restaurant Yemek &#199;eki ;, ;SetCard ;, ;MetropolCard 
-                                   ;, ;Sodexo Yemek &#199;eki ;, ;Sodexo Yemek Kartı ;, ;Kredi Kartı ;], ;PrimaryParentCategory ;: ; 
-                                   ;, ;PromotionInfoForFOC ;:null, ;PromotionInfo ;:[{ ;DiscountId ;:6150170, ;PromotionImageUrl ;: ;genelpromosyon.gif ;, ;PromotionText ;: 
-                                   ;Sadece Yemeksepeti\u0027nde, \u0027Se&#231;ilmiş Men&#252; (Et D&#246;ner D&#252;r&#252;m - 70 gr.)\u0027 22 TL yerine 19,5 TL! ;},
-                                   { ;DiscountId ;:6150171, ;PromotionImageUrl ;: ;null ;, ;PromotionText ;: ;Sadece Yemeksepeti\u0027nde, \u0027Se&#231;ilmiş Men&#252; 
-                                   (Kaşarlı K&#246;fte)\u0027 30 TL yerine 26,9 TL! ;}], ;PromotionInfoForPopup ;: ; ;, ;RestaurantSubState ;: ; ;, ;Serving ;:9.37, ;
-                                    Slug ;:null, ;Speed ;:9.09, ;WorkHoursText ;: ;08:00-23:59 ;, ;Refiners ;:{ ;AreasCount ;:[], ;CuisinesCount ;:[], ;HasDvdPromotionCount ;:0, 
-                                    ;HasPromotionCount ;:0, ;OpenRestaurantCount ;:0}, ;AvgPoint ;: ;9,2 ;, ;ImagePathSmall ;: 
-                                    ;/CategoryImages/TR_ISTANBUL/goreme_muhalllebicisi_kurtulus_small.gif ;, ;ImageFullPathSmall ;: ;
-                                    http://images.yemeksepeti.com//CategoryImages/TR_ISTANBUL/goreme_muhalllebicisi_kurtulus_small.gif ;, ;
-                                    SeoUrl ;: ;/goreme-muhallebicisi-acibadem-istanbul ;, ;CuisineImageList ;:[ ;Pasta.jpg ;, ;kahvalti.png ;, ;Cafe.jpg ;],
-                                     ;AllPromotionImageList ;:[ ;genelpromosyon.gif ;, ;cc_genel.gif ;], ;ServingText ;: ;9,4 ;, ;SpeedText ;: ;9,1 ;, ;FlavourText ;: ;9,2 ;,
-                                      ;AvgRestaurantScore ;: ;9,2 ;, ;AvgRestaurantScorePoint ;:9.2, ;MinimumDeliveryPriceText ;: ;5,00 ;, ;RestaurantSubStateText ;:null
-                                      , ;AvgPointHtmlClass ;: ;point11 ;, ;FlavourPointHtmlClass ;: ;nopoint ;, ;HasCampusDiscount ;:false, ;IsFreezoneRestaurant ;:false,
-                                       ;IsNew ;:false, ;IsYSDeliveryRestaurant ;:false, ;HasPromotion ;:true}">G&#246;reme Muhallebicisi, Acıbadem</span>'''
