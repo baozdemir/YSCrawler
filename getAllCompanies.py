ba4ed2ac0005ca71ff4106b057c4fcf57ab64aa8
Mesut Kutlu <mkutlu@etu.edu.tr>
2018-05-14 11:10:04 +0300

    city company list seperated into diff jsons programmatically.

--- getAllCompanies.py
+++ getAllCompanies.py
@@ -32,7 +32,7 @@ def getAllCompaniesFromZone():
                         print("{} city have {} {}".format(key,city_zone_length,key2))
                         print(zones_json['allZones'][count][key][count2][key2][0]['name'])
                         for zone in zones_json['allZones'][count][key][count2][key2]:
-                            page = requests.get('https://www.yemeksepeti.com{}'.format(zone['url']))
+                            page = requests.get('https://www.yemeksepeti.com{}/#ors:false'.format(zone['url']))
                             print(zone['url'])
                             soup = BeautifulSoup(page.content, 'html.parser')
                             companies = soup.find_all('a', class_='restaurantName')
@@ -56,17 +56,20 @@ def getAllCompaniesFromZone():
                                 href.append(company['href'])                       
                             aZone.append({ "comps" : [{"DisplayName" : dn, "CategoryName" : cn,"CuisineNameList" : cnl, "DeliveryTime" : dt,"MinimumDeliveryPrice" : mdp, "ImageFullPath" : ifp,"MainCuisineId" : mci, "MainCuisineName" : mcn,"ServingText": st, "SpeedText": spt,"FlavourText": ft,"AvgPoint" : ap, "WorkHoursText":wht, "href" : h} for dn,cn,cnl,dt,mdp,ifp,mci,mcn,st,spt,ft,ap,wht,h in zip(DisplayName,CategoryName,CuisineNameList,DeliveryTime,MinimumDeliveryPrice,ImageFullPath,MainCuisineId,MainCuisineName,ServingText,SpeedText,FlavourText,AvgPoint,WorkHoursText,href)]})
                             DisplayName,CategoryName,CuisineNameList,DeliveryTime,MinimumDeliveryPrice,ImageFullPath,MainCuisineId,MainCuisineName,ServingText,href,SpeedText,FlavourText,AvgPoint,WorkHoursText = [], [], [], [], [], [], [], [], [], [], [], [], [], []
-                            print(zone['name'])
                             cityZones.append({zone['name'] : aZone})
                             aZone = []
-                aCity.append({ key : cityZones })
-                cityZones = []
-    jsondata = {"allZones" : aCity}
-    print(jsondata)
-    with open('companies.json', 'w') as f:
-            json.dump(jsondata, f, ensure_ascii=False, indent=4, sort_keys=True)
+                aCity = ({ key : cityZones })
+                cityZones = []    
+                with open("json/{}_companies.json".format(key), 'w') as f:
+                    json.dump(aCity, f, ensure_ascii=False, indent=4, sort_keys=True)
 
 
                                 
 
 getAllCompaniesFromZone()
+
+
+                                
+
+getAllCompaniesFromZone()
+            
