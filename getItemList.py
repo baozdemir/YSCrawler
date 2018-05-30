15e25d518a9578972939b61abdfbc0a2388b356e
temizkanekin <emtemizkan@gmail.com>
2018-05-30 14:30:39 +0300

    getItemList sınıfı düzenlendi,permisyon lazım

--- getItemList.py
+++ getItemList.py
@@ -1,33 +1,41 @@
 import requests
 import json
 from bs4 import BeautifulSoup
-import glob, os
+import glob, os, os.path
 
 def getAllCompaniesFromZone(href):
-	page = requests.get('https://www.yemeksepeti.com{}'.format(href))
-	soup = BeautifulSoup(page.content, 'html.parser')
+	if(not(os.path.isfile("menusjson/{}.json".format(href)))):
+		page = requests.get('https://www.yemeksepeti.com{}'.format(href))
+		soup = BeautifulSoup(page.content, 'html.parser')
 
-	#//f = open ('out.txt', 'w')
-	#f.write(soup.prettify())
-	itemNames = soup.find_all('a', class_='getProductDetail')
-	productInfos = soup.find_all('span', class_='productInfo')
-	prices = soup.find_all('span', class_='pull-right newPrice')
+		#//f = open ('out.txt', 'w')
+		#f.write(soup.prettify())
+		itemNames = soup.find_all('a', class_='getProductDetail')
+		productInfos = soup.find_all('span', class_='productInfo')
+		prices = soup.find_all('span', class_='pull-right newPrice')
 
-	names, hrefs = [], []
-	product_info=[]
-	price_info=[]
-	i=0
-	for itemName in itemNames:
-		item_name = itemName.get_text()
-		for productInfo in productInfos:
-			product_info_p = productInfo.find('p')
-			product_info.append(product_info_p.get_text())
-			for price in prices:	
-				price_info.append(price.get_text())
-		print('{}'.format(item_name))
-		print('{}'.format(product_info[i]))
-		print('{}{}'.format(price_info[i],"\n"))
-		i=i+1
+		names, hrefs = [], []
+		item_names=[]
+		product_info=[]
+		price_info=[]
+		i=0
+		for itemName in itemNames:
+			item_name = itemName.get_text()
+			item_names.append(item_name)
+			for productInfo in productInfos:
+				product_info_p = productInfo.find('p')
+				product_info.append(product_info_p.get_text())
+				for price in prices:	
+					price_info.append(price.get_text())
+		#	print('{}'.format(item_name))
+		#	print('{}'.format(product_info[i]))
+		#	print('{}{}'.format(price_info[i],"\n"))
+			i=i+1
+		json_innerData = { "menu" : [{"Product_name" : p,"Product_info" : n, "Price_info" : u} for p,n,u in zip(item_names,product_info,price_info)]}
+		item_names,product_info, price_info = [], [],[]
+		with open("menusjson{}.json".format(href), 'w') as f:
+			json.dump(json_innerData, f, ensure_ascii=False, indent=4, sort_keys=True)
+
 os.chdir("json")
 for file in glob.glob("*.json"):
 	with open(file, 'r') as f:
