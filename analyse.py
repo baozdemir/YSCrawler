d02285a115b88352d25ca03f50e2ad541babd67c
Mesut Kutlu <mkutlu@etu.edu.tr>
2018-07-24 11:00:50 +0300

    json last

--- analyse.py
+++ analyse.py
@@ -12,7 +12,7 @@ def isfloat(value):
 
 directory = os.path.dirname(os.path.realpath(__file__))
 os.chdir("menusjson")
-for file in glob.glob("*ankara.json"):
+for file in glob.glob("*.json"):
     commentCounts = {}
     averagePoints = {}
     with open(file, 'r') as f:
